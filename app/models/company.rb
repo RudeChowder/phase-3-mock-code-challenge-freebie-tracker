30e2cfb62715c965ee212c43296851a6dfcfecb6
Stephen Crane <scrane88@gmail.com>
2021-08-30 10:27:35 -0400

    done

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,12 @@
 class Company < ActiveRecord::Base
+  has_many :freebies
+  has_many :devs, through: :freebies
 
+  def give_freebie(dev, item_name, value)
+    Freebie.create(company: self, dev: dev, item_name: item_name, value: value)
+  end
+
+  def self.oldest_company
+    order(:founding_year).limit(1)
+  end
 end
